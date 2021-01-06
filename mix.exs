07ff5036c9d5f2546a7bd8847d762b0f0eaba95b
Aaron Russell <aaron@gc4.co.uk>
2021-01-06 13:08:11 +0000

    Beta release.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Txpost.MixProject do
   def project do
     [
       app: :txpost,
-      version: "0.1.0-beta.1",
+      version: "0.1.0-beta.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
